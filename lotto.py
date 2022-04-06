268425484e112e1e91f2f3e5bed9b5f9cd9769cc
dongholee <ehdgh2287@partron.co.kr>
2022-04-06 19:08:53 +0900

    Added Functions and Exception Action

--- lotto.py
+++ lotto.py
@@ -1,135 +1,145 @@
 import requests
 import random
+import datetime
+
+def GetWinNumbers(drwNo):
+    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={drwNo}'
+    try:
+        json_result = requests.get(url).json()
+    except:
+        GetWinNumbers(drwNo)
+    if json_result.get('returnValue', 'fail') == 'fail': return None, None
+    res = []
+    res.append(json_result.get('drwtNo1', None))
+    res.append(json_result.get('drwtNo2', None))
+    res.append(json_result.get('drwtNo3', None))
+    res.append(json_result.get('drwtNo4', None))
+    res.append(json_result.get('drwtNo5', None))
+    res.append(json_result.get('drwtNo6', None))
+    res.append(json_result.get('bnusNo', None))
+    drwNoDate = json_result.get('drwNoDate', None)
+    return res, drwNoDate
+
+def GetCurrentCountFile():
+    try:
+        f = open('count.txt', 'r')
+    except FileNotFoundError:
+        print("[Error] count.txt 파일이 없습니다!!!")
+        return None
+    res = []
+    res.append(int(f.readline().split('=')[1]))
+    lines = f.readlines()    
+    for l in lines: res.append(int(l.split('=')[1].split('\n')[0]))
+    f.close()
+    if len(res) < 46:
+        print("[Error] count.txt 파일이 잘못되었습니다!!!")
+        return None
+    return res
 
 def UpdateCountFile():
     r = {x:0 for x in range(1,46)}
-    f = open("count.txt", "r")
-    line = f.readline()
-    lines = f.readlines()
-    for l in lines: r[int(l.split('=')[0])] = int(l.split('=')[1].split('\n')[0])
-    f.close()
+    f_res = GetCurrentCountFile()
+    if f_res == None: return
+    drwNo = f_res[0]
+    for i in range(1,46): r[i] = f_res[i]
 
-    i = int(line.split('=')[1])
     while 1:
-        url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={}'
-        i += 1
-        url = url.format(i)
-        try:
-            req_result = requests.get(url)
-        except:
-            i -=1; continue
-        json_result = req_result.json()
-        if json_result.get('returnValue', 'fail') == 'fail': break
-        r[json_result.get('drwtNo1', None)] += 1
-        r[json_result.get('drwtNo2', None)] += 1
-        r[json_result.get('drwtNo3', None)] += 1
-        r[json_result.get('drwtNo4', None)] += 1
-        r[json_result.get('drwtNo5', None)] += 1
-        r[json_result.get('drwtNo6', None)] += 1
-        r[json_result.get('bnusNo', None)] += 1
+        drwNo += 1
+        winnums, drwNoDate = GetWinNumbers(drwNo)
+        if winnums == None: break
+        for n in winnums: r[n] += 1
+
         f = open("count.txt", "w")
-        f.write(f"drwNo={i}\n")
+        f.write(f"drwNo={drwNo}\n")
         for k,v in r.items():
             f.write(f"{k}={v}\n")
         f.close()
-        print(f"drwNo={i} Success!")
-    return i-1
+        print(f"drwNo={drwNo} Success!")
+    return drwNo-1
 
 def GetRecentlyNum(drwNo):
     result = ""
+    drwNoDate = ""
     while 1:
-        url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={}'
-        url = url.format(drwNo)
-        req_result = requests.get(url)
-        json_result = req_result.json()
-        if json_result.get('returnValue', None) == 'success':
-            drwNoDate = json_result.get('drwNoDate', None)
-            result = "{} {} {} {} {} {} + {}"
-            result = result.format( json_result.get('drwtNo1', None),
-                                    json_result.get('drwtNo2', None),
-                                    json_result.get('drwtNo3', None),
-                                    json_result.get('drwtNo4', None),
-                                    json_result.get('drwtNo5', None),
-                                    json_result.get('drwtNo6', None),
-                                    json_result.get('bnusNo', None) )
-            drwNo += 1
-            continue
-        else: break
+        winnums, date = GetWinNumbers(drwNo)
+        if winnums == None: break
+        drwNoDate = date
+        result = f"{winnums[0]} {winnums[1]} {winnums[2]} {winnums[3]} {winnums[4]} {winnums[5]} + {winnums[6]}"
+        drwNo += 1
     drwNo -= 1
-    print("# Recent round information\n")
-    print("drwNo :", drwNo, "\ndrwNoDate :", drwNoDate, "\nLotto Num :",result)
+    print(f"[{drwNo} 회차]\n날짜 : {drwNoDate}\n당첨 번호 : {result}")
     return drwNo
 
 def GetNumSortedProbabilityBase():
-    try:
-        f = open('count.txt', 'r')
-    except FileNotFoundError:
-        print("Please make a file : count.txt")
-        return
-    f.readline()
-    lines = f.readlines()
-    f.close()
-    if len(lines) < 45:
-        print("Please make the valid file : count.txt")
-        return
+    f_res = GetCurrentCountFile()
+    if f_res == None: return
     d = dict()
-    for l in lines:
-        d[int(l.split('=')[0])] = int(l.split('=')[1].split('\n')[0])
+    for i in range(1,46): d[i] = f_res[i]
     sorted_d = sorted(d, key = lambda x : d[x], reverse=True)
-    print("# The 'count.txt' file contains the number of winnings for all numbers from round 1 to round 'drwNo'.")
-    print("# Referring to the 'count.txt' file, they are listed in order of the most winning numbers.\n")
-    print("[Probability-based Sort]", end="")
     for i in range(45):
-        if i % 9 == 0: print()
         print(sorted_d[i], end=" ")
 
-def GetRandomNum():
-    try:
-        f = open('count.txt', 'r')
-    except FileNotFoundError:
-        print("Please make a file : count.txt")
-        return
-    f.readline()
-    lines = f.readlines()
-    f.close()
-    if len(lines) < 45:
-        print("Please make the valid file : count.txt")
-        return
+def GetRandomNum(is_weight):
     w = []
-    for l in lines: w.append(float(l.split("=")[1].split("\n")[0]))
-
-    print("# The 'count.txt' file contains the number of winnings for all numbers from round 1 to round 'drwNo'.")
-    print("# The probability was calculated by referring to the 'count.txt' file.")
-    print("# random numbers were generated by applying each probability.\n")
+    result = []
+    if is_weight == True:
+        f_res = GetCurrentCountFile()
+        if f_res == None: return
+        for i in range(1,46): w.append(float(f_res[i]))
+    elif is_weight == False:
+        w = [1]*45
     for i in range(5):
         nums = []
         while len(nums) < 6:
             num = random.choices(range(1, 46), weights = w)
-            if num in nums: continue
-            else: nums.extend(num)
-        string = f"Set{i+1} : {nums[0]} {nums[1]} {nums[2]} {nums[3]} {nums[4]} {nums[5]}"
-        print(string)
+            if num[0] in nums: continue
+            else: nums.extend(num)        
+        result.append(nums)
+    return result
 
-def CmdInit():
-    print("\n==============================")
-    print("1. Get Lotto Random Number")
-    print("2. Get Lotto Recently Result")
-    print("3. Get Lotto Number Sorted by Probability")
-    print("4. Update the file (count.txt)")
-    print("0. Exit")
-    print("==============================")
+def CountTimeLoop(winnums, filename, drwNo, res):
+    cnt = 0
+    while 1:
+        cnt += 1
+        RanNums = GetRandomNum(False)
+        for rannum in RanNums:
+            rannum = sorted(rannum)
+            if rannum == winnums:
+                f = open(filename,'a')
+                f.write(f"\n{drwNo}={cnt}")
+                f.close()
+                res.append(cnt)
+                return
 
-def BackupFile():
-    try:
-        f = open('count.txt', 'r')
-        lines = f.readlines()
-    except FileNotFoundError:
-        f = open('count.txt', 'w')
-        f.write("drwNo=0")
-        lines = []
-    f.close()
+def GetExceCntToWin(StartNo, EndNo):
+    now = datetime.datetime.now()
+    date = now.strftime("%Y%m%d")
+    filename = f"No.{StartNo}_{EndNo}_{date}.txt"
+    drwNo = StartNo
+    res = []
+    while drwNo <= EndNo:
+        winnums, drwNoDate = GetWinNumbers(drwNo)
+        winnums = sorted(winnums[:6])
+        print(f"{drwNo}회 {winnums}")
+        CountTimeLoop(winnums, filename, drwNo, res)
+        drwNo += 1
+    print(f"{filename} 파일이 생성되었습니다.")
+    return min(res), max(res)
 
-    if len(lines) < 46: return
-    f = open('count_backup.txt', 'w')
-    for line in lines: f.write(line)
-    f.close()
+def GetRanNumNTimes(times):
+    now = datetime.datetime.now()
+    filename = now.strftime("%Y%m%d_%H%M.txt")
+    for i in range(times):
+        print(f"{i+1}번째 진행중")
+        data = ""
+        result = GetRandomNum(False)
+        for nums in result:
+            nums = sorted(nums)
+            data += f"{nums[0]},{nums[1]},{nums[2]},{nums[3]},{nums[4]},{nums[5]},"
+        data = data[:-1] + "\n"
+        f = open(filename, "a")
+        f.write(data)
+        f.close()
+        if i == times-1: res = data
+    print(f"{filename} 파일이 생성되었습니다.")
+    return res
--- main.py
+++ main.py
@@ -1,22 +1,123 @@
 from lotto import *
 import os
 
-if __name__ == "__main__":
-    BackupFile()
-    drwNo = 1009
+def CmdInit():
+    print("==============================")
+    print("1. 랜덤 번호 생성 - 5게임")
+    print("2. 최근 당첨 번호")
+    print("3. 당첨된 횟수 기준 번호 정렬 (count.txt)")
+    print("4. 파일 업데이트 (count.txt)")
+    print("5. 랜덤 번호 생성 - N번 (월일_시분.txt 파일)")
+    print("6. 당첨까지 돌린 횟수 (No.시작회차_종료회차_날짜.txt 파일)")
+    print("0. 종료")
+    print("==============================")
+    n = input(">> ")
+    os.system('cls')
+    return n
+
+def BackupFile():
+    try:
+        f = open('count.txt', 'r')
+        lines = f.readlines()
+        drwNo = int(lines[0].split("=")[1].split("\n")[0])
+        f.close()
+    except FileNotFoundError:
+        f = open('count.txt', 'w')
+        data = "drwNo=0\n"
+        for i in range(1,46): data += f"{i}=0\n"
+        f.write(data)
+        f.close()
+        return 0
+    f = open('count_backup.txt', 'w')
+    for line in lines: f.write(line)
+    f.close()
+    return drwNo
+
+def PrintRandomNum():
+    print("1. 가중치 적용")
+    print("2. 가중치 미적용")
+    print("0. 뒤로")
+    key = input(">> ")
+    os.system('cls')
+    if key == '1':
+        print("# 가중치 적용하여 난수 생성 (count.txt 파일 기준)")
+        result = GetRandomNum(True)
+        i=0
+        for nums in result: i += 1; print(f"{i}게임 : {nums[0]} {nums[1]} {nums[2]} {nums[3]} {nums[4]} {nums[5]}")
+    elif key == '2':
+        print("# 모두 동등한 확률을 가진 채로 난수 생성.")
+        result = GetRandomNum(False)
+        i=0
+        for nums in result: i += 1; print(f"{i}게임 : {nums[0]} {nums[1]} {nums[2]} {nums[3]} {nums[4]} {nums[5]}")
+    elif key == '0':
+        return 0
+    else:
+        print("[Error] 잘못된 번호 입력!!!")
+    return 1
+
+def PrintRanNumNTimes():
     while 1:
-        CmdInit()
-        n = input("Input Number : ")
-        os.system('cls')
+        print("0. 뒤로")
+        try:
+            times = int(input("돌릴 횟수를 입력해주세요 : "))
+            if times == 0: os.system("cls"); return 0
+            break
+        except:
+            os.system("cls")
+            print("[Error] 숫자를 입력해주세요.\n")
+            continue
+    result = GetRanNumNTimes(times)
+    print("\n", result.replace(',',' '))
+    return 1
+
+def PrintExceCntToWin():
+    while 1:
+        print("0. 뒤로")
+        try:
+            StartNo = int(input("시작회차를 입력해주세요 : "))
+            if StartNo == 0: os.system("cls"); return 0
+            str = f"0. 뒤로\n시작회차를 입력해주세요 : {StartNo}"
+            break
+        except:
+            os.system("cls")
+            print("[Error] 숫자를 입력해주세요.\n")
+            continue
+    while 1:
+        try:
+            EndNo = int(input("종료회차를 입력해주세요 : "))
+            if EndNo == 0: os.system("cls"); return 0
+            break
+        except:
+            os.system("cls")
+            print(f"[Error] 숫자를 입력해주세요.\n\n{str}")
+            continue
+    if StartNo > EndNo:
+        os.system("cls")
+        print("[Error] 시작회차 > 종료회차.\n\t시작회차에 더 작은 수를 입력해주세요.")
+        return PrintExceCntToWin()
+    min, max = GetExceCntToWin(StartNo, EndNo)
+    print(f"Min : {min}\nMax : {max}")
+    return 1
+
+if __name__ == "__main__":
+    drwNo = BackupFile()
+    while 1:
+        n = CmdInit()
         if n == '1':
-            GetRandomNum()
+            if PrintRandomNum() == 0: continue
         elif n == '2':
             drwNo = GetRecentlyNum(drwNo)
         elif n == '3':
             GetNumSortedProbabilityBase()
         elif n == '4':
             drwNo = UpdateCountFile()
+        elif n == '5':
+            if PrintRanNumNTimes() == 0: continue
+        elif n == '6':
+            if PrintExceCntToWin() == 0: continue
         elif n == '0':
             break
         else:
-            print("[Error] You just have put invalid number.\n\tPlease put valid input number.")
+            print("[Error] 잘못된 번호 입력!!!")
+        input("\n계속하시려면 Enter를 눌러주세요.")
+        os.system('cls')
